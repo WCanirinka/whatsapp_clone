039fdd1cfe72497e19d9d46bc2346b7ae98cdfa8
Wilfried Canirinka <canirinkawilfried@gmail.com>
2020-10-05 20:10:57 +0200

    Setup the app area

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -5,6 +5,11 @@ const Sidebar = () => {
   return (
     <div className='sidebar'>
       <h1>I am a sidebar</h1>
+      <div className="sidebar__header">
+        <div className="sidebar__headerRight">
+          
+        </div>
+      </div>
     </div>
   )
 }
