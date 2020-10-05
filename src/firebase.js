38336dd9087211abd8089fc79c749e6992cb6625
Wilfried Canirinka <canirinkawilfried@gmail.com>
2020-10-05 19:07:51 +0200

    Add initial files

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBQctoK0kjkQk45ztAbbTxdGf03iU1ZCnk",
+  authDomain: "whatsapp-mern-b7892.firebaseapp.com",
+  databaseURL: "https://whatsapp-mern-b7892.firebaseio.com",
+  projectId: "whatsapp-mern-b7892",
+  storageBucket: "whatsapp-mern-b7892.appspot.com",
+  messagingSenderId: "370982157196",
+  appId: "1:370982157196:web:b36831c00e7b87888eb4a9"
+};
